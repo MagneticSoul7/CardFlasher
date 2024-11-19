cad6e320a7be6c935270401fba74c1232919f3e2
Redwolf917 <xander0429@gmail.com>
2024-11-18 20:28:51 -0700

    base work set up

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+const typeDefs = require('./schemas/typeDefs');
+const resolvers = require('./schemas/resolvers');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/card-flasher', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}${server.graphqlPath}`);
+});
